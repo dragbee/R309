7e2134c86d4f35ae201249f7d6087412acef0df3
dragbee <mp.ketta@gmail.com>
2022-12-09 11:25:48 +0100

    update

--- SAE302/GUI.py
+++ SAE302/GUI.py
@@ -1,7 +1,9 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from Client import Client
+import sys
 
-
+"""
 class MainWindow(QMainWindow):
 
    def __init__(self):
@@ -38,3 +40,60 @@ class MainWindow(QMainWindow):
       grid.addWidget(self.__name)
       grid.addWidget(self.__text2)
       grid.addWidget(self.__csv)
+"""
+
+class TextEditDemo(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.i = 0
+        self.setWindowTitle("QTextEdit")
+        self.resize(300, 270)
+
+        self.connip = QLabel("IP")
+        self.ip = QLineEdit()
+        self.connport = QLabel("PORT")
+        self.port = QLineEdit()
+        self.conn = QPushButton("Connexion")
+        self.textEdit = QTextEdit()
+        self.textEdit.setEnabled(False)
+        self.btnPress1 = QPushButton("Add message")
+        self.text = QTextEdit()
+        self.btnPress2 = QPushButton("Clear")
+
+        layout = QGridLayout()
+        layout.addWidget(self.connip, 0, 0)
+        layout.addWidget(self.ip, 0, 1)
+        layout.addWidget(self.connport, 0, 2)
+        layout.addWidget(self.port, 0, 3)
+        layout.addWidget(self.conn, 0, 4)
+        layout.addWidget(self.textEdit, 1, 0, 1, 4)
+        layout.addWidget(self.text, 2, 0, 1, 4)
+        layout.addWidget(self.btnPress1, 3, 0)
+        layout.addWidget(self.btnPress2, 3, 1)
+
+        self.setLayout(layout)
+
+        self.btnPress1.clicked.connect(self.btnPress1_Clicked)
+        self.btnPress2.clicked.connect(self.btnPress2_Clicked)
+        self.conn.clicked.connect(self.client_connect)
+
+    def btnPress1_Clicked(self):
+        self.textEdit.append(f"{self.text.toPlainText()}")
+        self.text.clear()
+#        self.textEdit.setPlainText("Hello PyQt5!\nfrom pythonpyqt.com")
+
+    def btnPress2_Clicked(self):
+        self.textEdit.setPlainText("")
+#        self.textEdit.setHtml("<font color='red' size='6'><red>Hello PyQt5!\nHello</font>")
+
+    def client_connect(self, host, port):
+        self.client = Client.connect
+        return (self.client())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = TextEditDemo()
+    win.show()
+    sys.exit(app.exec_())
--- SAE302/Serveur.py
+++ SAE302/Serveur.py
@@ -1,6 +1,8 @@
 import socket
 import os
 import platform
+import subprocess
+import psutil
 
 
 def data():
@@ -8,17 +10,39 @@ def data():
     data = ""  # message client
     while data != "kill":
         server_socket = socket.socket()
-        server_socket.bind(('127.0.0.1', 8111))
+        server_socket.bind(('0.0.0.0', 8111))
         server_socket.listen(1)
         while data != "kill" and data != "reset":
             conn, address = server_socket.accept()
             while data != "disconnect" and data != "kill" and data != "reset":
                 data = conn.recv(1024).decode()
                 print("Client : ", data)
-                conn.send(data.encode())
-                if data == "OS":
-                    print("GEGE")
-                    conn.send(reply.encode())
+
+                if data == "OS" or data == "IP" or data == "NAME":
+                    if data == "OS":
+                        cmd = "ver"
+
+                    elif data == "IP":
+                        cmd = "ipconfig | findstr IPv4"
+
+                    else:  # data == "NAME"
+                        cmd = "hostname"
+
+                    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='cp850',
+                                         shell=True)
+                    cmd = p.stdout.read()
+                    conn.send(cmd.encode())
+
+                elif data == "RAM" or data == "CPU":
+                    if data == "RAM":
+                        ram = f"total : {psutil.virtual_memory()[0]} available : {psutil.virtual_memory()[1]} used : {psutil.virtual_memory()[3]}"
+                        conn.send(ram.encode())
+                    else: #data == "CPU"
+                        cpu = f"used (%) : {psutil.cpu_percent()}"
+                        conn.send(cpu.encode())
+                else:
+                    conn.send(data.encode())
+
             conn.close()
         server_socket.close()  # fermeture serveur
 
