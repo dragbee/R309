8abf86c229ee6c65483cb3ccfe36dfce3138414a
dragbee <92865220+dragbee@users.noreply.github.com>
2022-12-16 19:00:21 +0100

    Update GUI.py
    final update

--- SAE302/GUI.py
+++ SAE302/GUI.py
@@ -1,9 +1,12 @@
 from PyQt5.QtCore import *
+from PyQt5.QtGui import QCloseEvent
 from PyQt5.QtWidgets import *
 from Client import Client
 import socket
 import sys
 import threading
+import csv
+
 
 """
 class MainWindow(QMainWindow):
@@ -49,12 +52,12 @@ class TextEditDemo(QWidget):
         super().__init__(parent)
 
         self.i = 0
-        self.setWindowTitle("︻╦╤─")
+        self.setWindowTitle("Mon appli communicante")
         self.resize(500, 470)
         self.connexion = Client("127.0.0.1", 8111)
 
         self.connip = QLabel("IP")
-        self.ip = QLineEdit("127.0.0.1")
+        self.ip = QComboBox()
         self.connport = QLabel("PORT")
         self.port = QLineEdit("8111")
         self.conn = QPushButton("Connexion")
@@ -70,6 +73,7 @@ class TextEditDemo(QWidget):
         self.btnPress2.setEnabled(False)
         self.win = None
         self.button = QPushButton("Open Window")
+        self.csv = QPushButton("csv")
 
         layout = QGridLayout()
         layout.addWidget(self.connip, 0, 0)
@@ -84,6 +88,7 @@ class TextEditDemo(QWidget):
         layout.addWidget(self.btnPress1, 4, 0)
         layout.addWidget(self.btnPress2, 4, 1)
         layout.addWidget(self.button, 5, 0)
+        layout.addWidget(self.csv, 5, 4)
 
 
         self.setLayout(layout)
@@ -93,6 +98,7 @@ class TextEditDemo(QWidget):
         self.conn.clicked.connect(self.client_connect)
         self.deconn.clicked.connect(self.client_disconnect)
         self.button.clicked.connect(self.open_window)
+        self.csv.clicked.connect(self.convertcsv)
 
     def envoie(self):
         msg = self.texto.text()
@@ -115,41 +121,79 @@ class TextEditDemo(QWidget):
 
 
     def client_connect(self):
-        errc = QMessageBox()
-        errc.setWindowTitle("ERREUR")
-        errc.setText("Adresse ou port non valide, assurez-vous d'avoir la bonne adresse ou le bon port")
-
         try:
             self.socket = socket.socket()
-            ip = str(self.ip.text())
+            ip = str(self.ip.currentText())
             port = int(self.port.text())
             self.socket.connect((ip, port))
-            self.connect.setText("connecté")
+        except ConnectionRefusedError:
+            cre = QMessageBox()
+            cre.setWindowTitle("ERREUR")
+            cre.setText("Le serveur n'est pas lancé")
+            cre.exec()
+        except OSError:
+            errc = QMessageBox()
+            errc.setWindowTitle("ERREUR")
+            errc.setText("Adresse ou port non valide, assurez-vous d'avoir la bonne adresse ou le bon port")
+            errc.exec()
+        else:
+            self.connect.setText("Connecté")
             self.conn.setEnabled(False)
             self.btnPress1.setEnabled(True)
             self.btnPress2.setEnabled(True)
             self.deconn.setEnabled(True)
-        except:
-            errc.exec()
 
 
 
-    def client_disconnect(self):
-        self.connect.setText("déconnecté")
-        self.conn.setEnabled(True)
-        self.btnPress1.setEnabled(False)
-        self.btnPress2.setEnabled(False)
-        self.deconn.setEnabled(False)
-        self.textEdit.clear()
-        self.socket.close()
+    def client_disconnect(self, conn):
+        msg = "disconnect"
+        try:
+            self.socket.send(msg.encode())
+            self.socket.close()
+        except ConnectionAbortedError:
+            cae = QMessageBox
+            cae.setWindowTitle("zqds")
+            cae.setText("dsqdsqdqsd")
+            cae.exec()
+        except ConnectionResetError:
+            cre = QMessageBox
+            cre.setWindowTitle("zqds")
+            cre.exec()
+        else:
+            self.connect.setText("Déconnecté")
+            self.conn.setEnabled(True)
+            self.btnPress1.setEnabled(False)
+            self.btnPress2.setEnabled(False)
+            self.deconn.setEnabled(False)
 
 
     def open_window(self):
         self.win = TextEditDemo()
         self.win.show()
 
+    def closeEvent(self, _e: QCloseEvent):
+        box = QMessageBox()
+        box.setWindowTitle("Quitter ?")
+        box.setText("Voulez vous quitter ?")
+        box.addButton(QMessageBox.Yes)
+        box.addButton(QMessageBox.No)
 
+        ret = box.exec()
 
+        if ret == QMessageBox.Yes:
+            QCoreApplication.exit(0)
+        else:
+            _e.ignore()
+
+    def convertcsv(self):
+        filename = QFileDialog.getOpenFileName()
+        path = str(filename[0])
+        with open(path, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                ips = row
+                print(row[0])
+            self.ip.addItems(row)
 
 
 if __name__ == '__main__':
